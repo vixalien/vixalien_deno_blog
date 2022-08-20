edd6732ed2a789c458748055b23b43a1e69e4ad9
Angelo Verlain <geoangercola@gmail.com>
2022-08-20 06:58:52 +0200

    fix middlewares not working when hosted

--- markedMiddleware/util.ts
+++ markedMiddleware/util.ts
@@ -1,7 +1,14 @@
 import { dirname, fromFileUrl, resolve } from "../deps.ts";
 
 export const load = (path: string) => {
-  return Deno.readTextFile(
-    resolve(dirname(fromFileUrl(import.meta.url)), path),
-  );
+  const base = import.meta.url;
+  const protocol = new URL(base).protocol;
+
+  if (protocol === "file:") {
+    return Deno.readTextFile(
+      resolve(dirname(fromFileUrl(base)), path),
+    );
+  } else {
+    return fetch(new URL(path, base)).then((response) => response.text);
+  }
 };
